96aba9299b2f7b2874e21ee1b371133b71903346
somes <admin@somes.me>
2026-06-29 20:21:26 +0800

    feat(rust): align with existing code style

--- wrapper/rust/wolfssl-wolfcrypt/src/sm2.rs
+++ wrapper/rust/wolfssl-wolfcrypt/src/sm2.rs
@@ -214,9 +214,9 @@ impl SM2 {
     /// let rng = RNG::new().expect("Failed to create RNG");
     /// let mut sm2 = SM2::generate(&rng, SM2::FLAG_NONE, None, None).expect("Error with generate()");
     /// let hash = [0x42u8; 32];
-    /// let mut signature = [0u8; 128];
-    /// let signature_length = sm2.sign_hash(&hash, &mut signature, &rng).expect("Error with sign_hash()");
-    /// let signature = &signature[..signature_length];
+    /// let mut signature = [0u8; 73];
+    /// let signature_len = sm2.sign_hash(&hash, &mut signature, &rng).expect("Error with sign_hash()");
+    /// let signature = &signature[..signature_len];
     /// let mut d = [0u8; 32];
     /// let d_size = sm2.export_private(&mut d).expect("Error with export_private()");
     /// let mut x963 = [0u8; 128];
@@ -261,20 +261,21 @@ impl SM2 {
     /// #[cfg(all(ecc_import, ecc_curve_sm2p256v1))]
     /// {
     /// use wolfssl_wolfcrypt::sm2::SM2;
+    /// use wolfssl_wolfcrypt::sys;
     ///
     /// // ECC key
     /// let qx = b"7a4e287890a1a47ad3457e52f2f76a83ce46cbc947616d0cbaa82323818a793d\0";
     /// let qy = b"eec4084f5b29ebf29c44cce3b3059610922f8b30ea6e8811742ac7238fe87308\0";
     /// let d  = b"8c14b793cb19137e323a6d2e2a870bca2e7a493ec1153b3a95feb8a4873f8d08\0";
     /// let mut key = SM2::import_raw(qx, qy, d, None, None).expect("Error with import_raw()");
-    /// assert!(key.check().is_err());
+    /// assert_eq!(key.check(), Err(sys::wolfCrypt_ErrorCodes_MP_VAL));
     ///
     /// // SM2 key from `certs/sm2/client-sm2-priv.pem`.
     /// let qx = b"3a1de8cb4bd32e3f4b073fb021fec59ed9ca3a939395761d30d90bf556ed1960\0";
     /// let qy = b"ed014cf6671df1aca8740db277c84938e4ff4cef8d6d87f64ec7f839747070b5\0";
     /// let d  = b"d0a2df497a2ddf02c9ceb7f237020dddfc08b8de14937a532649d5fe02d9f371\0";
     /// let mut key = SM2::import_raw(qx, qy, d, None, None).expect("Error with import_raw()");
-    /// assert!(key.check().is_ok());
+    /// key.check().expect("Error with check()");
     /// }
     /// ```
     #[cfg(all(ecc_import, ecc_curve_sm2p256v1))]
